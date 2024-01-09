c9dbd97d5e69ffa42c670e7fb6f32d6a7e238cc6
Baelrin <Baelrin-Revel@yandex.ru>
2024-01-09 22:23:26 +0300

    Create basic structure \ connection with database

--- master.py
+++ master.py
@@ -1,1 +1,34 @@
+from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
+from langchain.memory import CassandraChatMessageHistory, ConversationBufferMemory
+import json
 
+cloud_config = {
+    'secure_connect_bundle': 'secure-connect-the-forgotten-realms.zip'
+}
+
+with open("The_Forgotten_Realms-token.json") as f:
+    secrets = json.load(f)
+
+CLIENT_ID = secrets["clientId"]
+CLIENT_SECRET = secrets["secret"]
+ASTRA_DB_KEYSPACE = 'database'
+OPENAI_API_KEY = ''
+
+auth_provider = PlainTextAuthProvider(CLIENT_ID, CLIENT_SECRET)
+cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider)
+session = cluster.connect()
+
+message_history = CassandraChatMessageHistory(
+    session_id='my_session',
+    session=session,
+    keyspace=ASTRA_DB_KEYSPACE,
+    ttl_second=3600
+)
+
+message_history.clear()
+
+cass_buff_memory = ConversationBufferMemory(
+    memory_key='chat_history',
+    chat_memory=message_history
+)
